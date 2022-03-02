cc16743cb3ffc524e2139a6bd17de01594fb4d0f
Ankur Seth <iankurseth@gmail.com>
2022-03-03 05:29:00 +0530

    Corrected Header and Footer

--- src/Reded/Reded.js
+++ src/Reded/Reded.js
@@ -6,12 +6,16 @@ export default function Reded () {
 
 	return (
 		<div className="Reded">
-			<header className="min-h-screen bg-red-500 text-white px-4 py-4">
-				<h2 className="m-auto text-xl font-bold py-4">Reded</h2>
-				<Editor />
+			<header className="bg-red-600 text-white px-4 py-2">
+				<div className="max-w-4xl mx-auto">
+					<h2 className="m-auto text-base font-bold py-2">Reded</h2>
+				</div>
 			</header>
-			<footer className="flex bg-slate-700 py-10 px-4">
-				<div className="text-white text-center">
+			<div className="min-h-screen bg-red-500 text-white px-4 py-8">
+				<Editor />
+			</div>
+			<footer className="bg-slate-700 px-4 py-16">
+				<div className="max-w-4xl mx-auto text-white text-center">
 					<h2 className="text-base font-bold"><a href="https://github.com/iaseth/reded" target="_blank" rel="noreferrer">Reded</a> by Ankur Seth.</h2>
 				</div>
 			</footer>
